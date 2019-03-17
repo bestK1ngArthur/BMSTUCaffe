313225883019c7ac83d4ff1f9de5f60b4b724ff3
Artem Belkov <bestk1ngarthur@aol.com>
2019-03-17 13:25:06 +0300

    Add base models

---
+++ BMSTUCaffe/Models/Caffe.swift
@@ -0,0 +1,13 @@
+//
+//  Caffe.swift
+//  BMSTUCaffe
+//
+//  Created by a.belkov on 17/03/2019.
+//  Copyright © 2019 bestK1ng. All rights reserved.
+//
+
+import Foundation
+
+struct Caffe {
+    var name: String
+}
---
+++ BMSTUCaffe/Models/Dish.swift
@@ -0,0 +1,15 @@
+//
+//  Dish.swift
+//  BMSTUCaffe
+//
+//  Created by a.belkov on 17/03/2019.
+//  Copyright © 2019 bestK1ng. All rights reserved.
+//
+
+import Foundation
+
+struct Dish {
+    var outlet: String
+    var name: String
+    var price: String
+}
