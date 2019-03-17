04af3c513d0878ec0f3a8536878992cef39470e2
Artem Belkov <bestk1ngarthur@aol.com>
2019-03-17 12:24:03 +0300

    Initial Commit

---
+++ BMSTUCaffe/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BMSTUCaffe
+//
+//  Created by a.belkov on 17/03/2019.
+//  Copyright © 2019 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMSTUCaffe/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BMSTUCaffe
+//
+//  Created by a.belkov on 17/03/2019.
+//  Copyright © 2019 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
